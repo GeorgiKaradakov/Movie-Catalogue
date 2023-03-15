263f3082a1d36f242a96f3ed06daf8ebe8ccb44d
StefanIanovski <notstefu@gmail.com>
2023-03-15 20:35:23 +0200

    temporary forms made

--- Register.cs
+++ Register.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace Movie_Database
 {
-    public partial class Form1 : Form
+    public partial class Register : Form
     {
-        public Form1()
+        public Register()
         {
             InitializeComponent();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
