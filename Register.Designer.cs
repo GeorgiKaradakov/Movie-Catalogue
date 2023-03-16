ac91b591aaa9b61c1c9b64c7d8009e00ceda6df0
StefanIanovski <notstefu@gmail.com>
2023-03-16 15:12:21 +0200

    Remade temporary design

--- Register.Designer.cs
+++ Register.Designer.cs
@@ -28,64 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.passwordlbl = new System.Windows.Forms.Label();
+            this.usrnamelbl = new System.Windows.Forms.Label();
+            this.passwordReg = new System.Windows.Forms.TextBox();
+            this.usernameReg = new System.Windows.Forms.TextBox();
+            this.registerButt = new System.Windows.Forms.Button();
+            this.confirmPassReg = new System.Windows.Forms.TextBox();
+            this.confirmPasslbl = new System.Windows.Forms.Label();
+            this.emailReg = new System.Windows.Forms.TextBox();
+            this.emaillbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label2
+            // passwordlbl
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(336, 135);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 13);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Password:";
+            this.passwordlbl.AutoSize = true;
+            this.passwordlbl.Location = new System.Drawing.Point(334, 210);
+            this.passwordlbl.Name = "passwordlbl";
+            this.passwordlbl.Size = new System.Drawing.Size(56, 13);
+            this.passwordlbl.TabIndex = 7;
+            this.passwordlbl.Text = "Password:";
             // 
-            // label1
+            // usrnamelbl
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(336, 62);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Username:";
+            this.usrnamelbl.AutoSize = true;
+            this.usrnamelbl.Location = new System.Drawing.Point(334, 118);
+            this.usrnamelbl.Name = "usrnamelbl";
+            this.usrnamelbl.Size = new System.Drawing.Size(58, 13);
+            this.usrnamelbl.TabIndex = 6;
+            this.usrnamelbl.Text = "Username:";
             // 
-            // textBox2
+            // passwordReg
             // 
-            this.textBox2.Location = new System.Drawing.Point(318, 160);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 5;
+            this.passwordReg.Location = new System.Drawing.Point(257, 235);
+            this.passwordReg.Name = "passwordReg";
+            this.passwordReg.Size = new System.Drawing.Size(215, 20);
+            this.passwordReg.TabIndex = 5;
             // 
-            // textBox1
+            // usernameReg
             // 
-            this.textBox1.Location = new System.Drawing.Point(318, 87);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 4;
+            this.usernameReg.Location = new System.Drawing.Point(257, 148);
+            this.usernameReg.Name = "usernameReg";
+            this.usernameReg.Size = new System.Drawing.Size(215, 20);
+            this.usernameReg.TabIndex = 4;
             // 
-            // button1
+            // registerButt
             // 
-            this.button1.Location = new System.Drawing.Point(329, 245);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Register";
-            this.button1.UseVisualStyleBackColor = true;
+            this.registerButt.Location = new System.Drawing.Point(319, 366);
+            this.registerButt.Name = "registerButt";
+            this.registerButt.Size = new System.Drawing.Size(75, 23);
+            this.registerButt.TabIndex = 8;
+            this.registerButt.Text = "Register";
+            this.registerButt.UseVisualStyleBackColor = true;
+            // 
+            // confirmPassReg
+            // 
+            this.confirmPassReg.Location = new System.Drawing.Point(257, 307);
+            this.confirmPassReg.Name = "confirmPassReg";
+            this.confirmPassReg.Size = new System.Drawing.Size(215, 20);
+            this.confirmPassReg.TabIndex = 9;
+            // 
+            // confirmPasslbl
+            // 
+            this.confirmPasslbl.AutoSize = true;
+            this.confirmPasslbl.Location = new System.Drawing.Point(316, 281);
+            this.confirmPasslbl.Name = "confirmPasslbl";
+            this.confirmPasslbl.Size = new System.Drawing.Size(93, 13);
+            this.confirmPasslbl.TabIndex = 10;
+            this.confirmPasslbl.Text = "Confirm password:";
+            // 
+            // emailReg
+            // 
+            this.emailReg.Location = new System.Drawing.Point(257, 59);
+            this.emailReg.Name = "emailReg";
+            this.emailReg.Size = new System.Drawing.Size(215, 20);
+            this.emailReg.TabIndex = 11;
+            // 
+            // emaillbl
+            // 
+            this.emaillbl.AutoSize = true;
+            this.emaillbl.Location = new System.Drawing.Point(334, 29);
+            this.emaillbl.Name = "emaillbl";
+            this.emaillbl.Size = new System.Drawing.Size(38, 13);
+            this.emaillbl.TabIndex = 12;
+            this.emaillbl.Text = "E-mail:";
             // 
             // Register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.emaillbl);
+            this.Controls.Add(this.emailReg);
+            this.Controls.Add(this.confirmPasslbl);
+            this.Controls.Add(this.confirmPassReg);
+            this.Controls.Add(this.registerButt);
+            this.Controls.Add(this.passwordlbl);
+            this.Controls.Add(this.usrnamelbl);
+            this.Controls.Add(this.passwordReg);
+            this.Controls.Add(this.usernameReg);
             this.Name = "Register";
             this.Text = "Register";
             this.ResumeLayout(false);
@@ -95,10 +135,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label passwordlbl;
+        private System.Windows.Forms.Label usrnamelbl;
+        private System.Windows.Forms.TextBox passwordReg;
+        private System.Windows.Forms.TextBox usernameReg;
+        private System.Windows.Forms.Button registerButt;
+        private System.Windows.Forms.TextBox confirmPassReg;
+        private System.Windows.Forms.Label confirmPasslbl;
+        private System.Windows.Forms.TextBox emailReg;
+        private System.Windows.Forms.Label emaillbl;
     }
 }
